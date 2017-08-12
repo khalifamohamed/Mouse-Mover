d6aa690d8b5a99fce9e9c7679db16ecd930afa81
Mohamed Khalifa <30705507+khalifamohamed@users.noreply.github.com>
2017-08-12 02:24:06 +0200

    Update MUI.java

--- MUI.java
+++ MUI.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mouse.mover;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 /**
  *
- * @author Khalifa
+ * @author khalifamohamed
  */
 public class MUI extends JFrame implements  KeyListener{
 JLabel closeLabel = new JLabel("Press the \"x\" key to close me!");
